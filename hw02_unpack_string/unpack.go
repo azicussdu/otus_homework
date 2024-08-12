b1bb0ff428abd8967bacf4bd9bd7a38e24cf011e
Azamat T <azicus.sdu@gmail.com>
2024-08-12 15:24:07 +0500

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,68 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(str string) (string, error) {
+	var builder strings.Builder
+	if len(str) == 0 {
+		return "", nil
+	}
+
+	if unicode.IsDigit(rune(str[0])) {
+		return "", ErrInvalidString
+	}
+
+	slashIsOpen := false
+	for ind, chr := range str {
+		lastChar := ""
+		if builder.Len() > 0 {
+			lastChar = string(builder.String()[builder.Len()-1])
+		}
+
+		switch {
+		case unicode.IsLetter(chr) || chr == '\n':
+			builder.WriteString(string(chr))
+		case chr == '\\':
+			if !slashIsOpen {
+				slashIsOpen = true
+			} else {
+				builder.WriteString(string(chr))
+				slashIsOpen = false
+			}
+		case unicode.IsDigit(chr):
+			switch {
+			case chr == '0':
+				// if digit is 0 then remove last letter from resulting string
+				removeLastChar(&builder)
+			case slashIsOpen:
+				builder.WriteString(string(chr))
+				slashIsOpen = false
+			case len(str) > ind+1 && unicode.IsDigit(rune(str[ind+1])):
+				return "", ErrInvalidString
+			default:
+				nTimes, err := strconv.Atoi(string(chr))
+				if err == nil {
+					builder.WriteString(strings.Repeat(lastChar, nTimes-1))
+				}
+			}
+		}
+	}
+
+	return builder.String(), nil
+}
+
+func removeLastChar(b *strings.Builder) {
+	if b.Len() == 0 {
+		return
+	}
+	str := b.String()
+	str = str[:len(str)-1]
+	b.Reset()
+	b.WriteString(str)
 }
--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -4,7 +4,7 @@ import (
 	"errors"
 	"testing"
 
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/require" //nolint:depguard
 )
 
 func TestUnpack(t *testing.T) {
@@ -16,11 +16,14 @@ func TestUnpack(t *testing.T) {
 		{input: "abccd", expected: "abccd"},
 		{input: "", expected: ""},
 		{input: "aaa0b", expected: "aab"},
+		{input: "d\n5abc", expected: "d\n\n\n\n\nabc"},
+		{input: "", expected: ""},
 		// uncomment if task with asterisk completed
-		// {input: `qwe\4\5`, expected: `qwe45`},
-		// {input: `qwe\45`, expected: `qwe44444`},
-		// {input: `qwe\\5`, expected: `qwe\\\\\`},
-		// {input: `qwe\\\3`, expected: `qwe\3`},
+		{input: `qwe\4\5`, expected: `qwe45`},
+		{input: `qwe\45`, expected: `qwe44444`},
+		{input: `qwe\\5`, expected: `qwe\\\\\`},
+		{input: `qwe\\\3`, expected: `qwe\3`},
+		{input: `otus\\5\4\\\2`, expected: `otus\\\\\4\2`},
 	}
 
 	for _, tc := range tests {
